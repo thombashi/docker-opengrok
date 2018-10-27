c6fe8f5e1f7394fbd6b94b83ca074fb299e1b2d4
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-27 11:04:53 +0900

    Fix inotify wait loop

--- run_opengrok.sh
+++ run_opengrok.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-INOTIFY_CMD="inotifywait -mr -e CLOSE_WRITE $OPENGROK_SRC_ROOT"
+INOTIFY_CMD="inotifywait --recursive --event close_write $OPENGROK_SRC_ROOT"
 REINDEX_GRACE_PERIOD=10  # [secs]
 REINDEX_INTERVAL=20  # [secs]
 
@@ -10,16 +10,16 @@ DATE_CMD="date --rfc-3339=seconds"
 
 catalina.sh run &
 
-${OPENGROK_INSTANCE_BASE}/bin/OpenGrok index $OPENGROK_SRC_ROOT
 echo "----- $($DATE_CMD): source code indexing -----" 1>&2
+${OPENGROK_INSTANCE_BASE}/bin/OpenGrok index $OPENGROK_SRC_ROOT
 
 while true; do
-    $INOTIFY_CMD | while read _; do
+    while $INOTIFY_CMD ; do
         echo "----- $($DATE_CMD): reindex grace period: $REINDEX_GRACE_PERIOD secs -----" 1>&2
         sleep $REINDEX_GRACE_PERIOD
 
-        ${OPENGROK_INSTANCE_BASE}/bin/OpenGrok index $OPENGROK_SRC_ROOT
         echo "----- $($DATE_CMD): reindexing -----" 1>&2
+        ${OPENGROK_INSTANCE_BASE}/bin/OpenGrok index $OPENGROK_SRC_ROOT
 
         # discard changes during the grace period and the reindexing
         break
