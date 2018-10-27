24b40f11ef862fe4eefa0756efee05c1bc21b059
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-27 10:58:31 +0900

    Add logging datetime to log messages

--- run_opengrok.sh
+++ run_opengrok.sh
@@ -6,24 +6,25 @@ INOTIFY_CMD="inotifywait -mr -e CLOSE_WRITE $OPENGROK_SRC_ROOT"
 REINDEX_GRACE_PERIOD=10  # [secs]
 REINDEX_INTERVAL=20  # [secs]
 
+DATE_CMD="date --rfc-3339=seconds"
 
 catalina.sh run &
 
-echo "----- source code indexing -----" 1>&2
 ${OPENGROK_INSTANCE_BASE}/bin/OpenGrok index $OPENGROK_SRC_ROOT
+echo "----- $($DATE_CMD): source code indexing -----" 1>&2
 
 while true; do
     $INOTIFY_CMD | while read _; do
-        echo "----- reindex grace period: $REINDEX_GRACE_PERIOD secs -----" 1>&2
+        echo "----- $($DATE_CMD): reindex grace period: $REINDEX_GRACE_PERIOD secs -----" 1>&2
         sleep $REINDEX_GRACE_PERIOD
 
-        echo "----- reindexing: $(date --rfc-3339=seconds) -----" 1>&2
         ${OPENGROK_INSTANCE_BASE}/bin/OpenGrok index $OPENGROK_SRC_ROOT
+        echo "----- $($DATE_CMD): reindexing -----" 1>&2
 
         # discard changes during the grace period and the reindexing
         break
     done
 
-    echo "----- reindex interval: $REINDEX_INTERVAL secs -----" 1>&2
+    echo "----- $($DATE_CMD): reindex interval: $REINDEX_INTERVAL secs -----" 1>&2
     sleep $REINDEX_INTERVAL
 done
